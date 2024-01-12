f41f52c78f4443738340e39adf23b9c156d0fd67
seoeunseol <ses6051@gmail.com>
2024-01-12 14:52:49 +0900

    no message

--- 2525/main.cpp
+++ 2525/main.cpp
@@ -1,24 +1,13 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    string a, b, bigger;
-
-    cin >> a >> b;
-
-    for (int i = 2; i >= 0; i--) {
-        if (a[i] > b[i]) {
-            bigger = a;
-            break;
-        }
-        else if (a[i] < b[i]) {
-            bigger = b;
-            break;
-        }
-        else {
-            continue;
-        }
-    }
-
-    cout << bigger;
-}
+/**
+* change first
+* change first
+* change first
+* change first
+* change first
+* change first
+* change first
+* change first
+* change first
+* change first
+* change first
+*/ 
